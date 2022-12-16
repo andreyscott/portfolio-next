6abe3cdc6a7d4f93f51fc8b74f8455d2aaa2c737
andreyscott <andreyscott301@gmail.com>
2022-12-16 23:21:48 +0100

    working on a solution

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,21 @@
 import type { GetStaticProps } from "next";
 import Head from "next/head";
-import Link from "next/link";
-import About from "../components/About";
-import ContactMe from "../components/ContactMe";
-import Header from "../components/Header";
-import Hero from "../components/Hero";
-import Projects from "../components/Projects";
-import Skiller from "../components/Skills/";
+// import Link from "next/link";
+// import About from "../components/About";
+// import ContactMe from "../components/ContactMe";
+// import Header from "../components/Header";
+// import Hero from "../components/Hero";
+// import Projects from "../components/Projects";
+// import Skiller from "../components/Skills/";
 // import Skills from "../components/Skills";
-import WorkExperience from "../components/WorkExperience";
-import { urlFor } from "../sanity";
+// import WorkExperience from "../components/WorkExperience";
+// import { urlFor } from "../sanity";
 import { Experience, PageInfo, Project, Skill, Social } from "../typings";
-import { fetchExperiences } from "../utils/fetchExperiences";
-import { fetchPageInfo } from "../utils/fetchPageInfo";
-import { fetchProjects } from "../utils/fetchProjects";
-import { fetchSkills } from "../utils/fetchSkills";
-import { fetchSocials } from "../utils/fetchSocials";
+// import { fetchExperiences } from "../utils/fetchExperiences";
+// import { fetchPageInfo } from "../utils/fetchPageInfo";
+// import { fetchProjects } from "../utils/fetchProjects";
+// import { fetchSkills } from "../utils/fetchSkills";
+// import { fetchSocials } from "../utils/fetchSocials";
 
 type Props = {
   pageInfo: PageInfo;
@@ -25,40 +25,19 @@ type Props = {
   socials: Social[];
 };
 
-const Home = ({ pageInfo, experiences, skill, projects, socials }: Props) => {
+const Home = () => {
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#4ff924]/80">
       <Head>
-        <title>{pageInfo?.name}</title>
+        {/* <title>{pageInfo?.name}</title> */}
+        <title>Home</title>
       </Head>
 
-      <Header socials={socials} />
-      {/* Hero */}
-      <section id="hero" className="snap-start">
-        <Hero pageInfo={pageInfo} />
-    </section>
-      {/* About */}
-      <section id="about" className="snap-center">
-        <About pageInfo={pageInfo} />
-      </section>
-      <section id="skills" className="snap-center">        
-        <Skiller />
-      </section>
-      {/* Experience */}
-      <section id="experience"  >
-        <WorkExperience experiences={experiences} />
-      </section>
-      {/* <section id="skills" className="snap-start">
-        <Skills skills={skill} />
-      </section> */}
+      <h1>
+        this feels dirty
+      </h1>
 
-      <section id="projects" className="snap-start">
-        <Projects projects={projects} />
-      </section>
 
-      <section id="contact" className="snap-start">
-        <ContactMe pageInfo={pageInfo} />
-      </section>
 
       {/* <Link href="#hero">
         <footer className="sticky bottom-5 w-full cursor-pointer">
@@ -78,24 +57,24 @@ const Home = ({ pageInfo, experiences, skill, projects, socials }: Props) => {
 
 export default Home;
 
-export const getStaticProps: GetStaticProps<Props> = async () => {
-  const pageInfo: PageInfo = await fetchPageInfo();
-  const experiences: Experience[] = await fetchExperiences();
-  const skill: Skill[] = await fetchSkills();
-  const projects: Project[] = await fetchProjects();
-  const socials: Social[] = await fetchSocials();
+// export const getStaticProps: GetStaticProps<Props> = async () => {
+//   const pageInfo: PageInfo = await fetchPageInfo();
+//   const experiences: Experience[] = await fetchExperiences();
+//   const skill: Skill[] = await fetchSkills();
+//   const projects: Project[] = await fetchProjects();
+//   const socials: Social[] = await fetchSocials();
 
-  return {
-    props: {
-      pageInfo,
-      experiences,
-      skill,
-      projects,
-      socials,
-    },
-    // Next.js will attempt to re-generate the page:
-    // - When a request comes in
-    // - At most once every 10 seconds
-    revalidate: 10,
-  };
-};
+//   return {
+//     props: {
+//       pageInfo,
+//       experiences,
+//       skill,
+//       projects,
+//       socials,
+//     },
+//     // Next.js will attempt to re-generate the page:
+//     // - When a request comes in
+//     // - At most once every 10 seconds
+//     revalidate: 10,
+//   };
+// };
--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -2,8 +2,10 @@ import { PageInfo } from "../typings";
 
 export const fetchPageInfo = async () => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`
-  );
+    // `${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`
+    `https://andrey.sanity.studio/desk/pageInfo`
+  )
+  
 
   const data = await res.json();
   const pageInfo: PageInfo = data.pageInfo;
